73044096c629fe8c7c8a5fd9be68465914a7c2ef
Kassidy <kqssidy@gmail.com>
2022-04-05 12:06:13 -0500

    Initial Commit

---
+++ API Calling3/API_Calling3App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling3App.swift
+//  API Calling3
+//
+//  Created by KSerek on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Calling3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  API Calling3
+//
+//  Created by KSerek on 4/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
